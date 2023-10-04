bd5f8ea039501fa2f498bb32551af89cf23abcef
JuanGonzalezJara <juan.m.gonzalez.j@gmail.com>
2023-10-03 22:51:28 -0300

    Feat: add css, scss, js and images to assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get "pages/index"
+  get "pages/gallery"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  #root "index#index"
+  root "pages#index"
 end
